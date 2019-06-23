b58ee311c3dfa65de52c6424014f043313a56826
shinya.hayashi <shinya.hayashi1103@gmail.com>
2019-06-23 08:37:44 +0000

    basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,16 +11,18 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = '新規作成に成功しました。'
+      log_in @user
+      flash[:success] = "ユーザーの新規作成に成功しました。"
       redirect_to @user
     else
-      render :new
+      render 'new'
     end
   end
 
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
 end
